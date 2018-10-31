0e2ea3e370770d51568780af4ca81888bd3dff0e
Michael Miles <michael@miles-me.net>
2018-10-31 13:12:40 -0500

    Prep for Drone Tild
    Found Code in the Unity Manual for tilting the gameobject as I have been planning. Removed redundant variables.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,8 @@ public class PlayerController : MonoBehaviour
     // Public Variables
     public float yawRate;
     public float speed;
-    public float forwardRotate;
-    public float sideSlipRotate;
-    public float forwardTilt;
-    public float sideTilt;
-    public float zRotate;
-    public float xRotate;
+    public float tiltAngle;
+    public float smoothTilt;
 
     // Private Variables
     private Rigidbody _droneRBody;
@@ -21,13 +17,7 @@ public class PlayerController : MonoBehaviour
 
     private void Start ()
     {
-        // Initiate Variables
-        //_droneRBody = GetComponent<Rigidbody>();
 
-        //yawRate = 60.0f;
-        //speed = 18.0f;
-        //forwardRotate = 12.0f;
-        //sideSlipRotate = 12.0f;
     }
 
     private void Awake()
@@ -37,8 +27,6 @@ public class PlayerController : MonoBehaviour
 
         yawRate = 60.0f;
         speed = 18.0f;
-        forwardRotate = 12.0f;
-        sideSlipRotate = 12.0f;
     }
     
     private void Update ()
@@ -49,16 +37,13 @@ public class PlayerController : MonoBehaviour
         var rStickX = Input.GetAxis("T1s_RStick-X");
         var rStickY = Input.GetAxis("T1s_RStick-Y");
 
-        xRotate = rStickY;
-        zRotate = rStickX;
+        //xRotate = rStickY;
+        //zRotate = rStickX;
 
         Vector3 movement = transform.TransformDirection(new Vector3(rStickX, verticalAxis, rStickY) * speed * Time.deltaTime);
         _droneRBody.MovePosition(transform.position + movement);
 
         //Quaternion rotation = Quaternion.Euler(new Vector3(0, horizontalAxis, 0) * yawRate * Time.deltaTime);
         transform.Rotate(new Vector3(0, horizontalAxis, 0), yawRate * Time.deltaTime);
-
-        _droneRotation.x = forwardRotate * rStickY;
-        _droneRotation.z = sideSlipRotate * rStickX;
     }
 }
